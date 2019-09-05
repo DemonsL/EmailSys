a91cb5d92ee1148beb920c9942fcdacfa0e9f322
luomurong <coolpad@yulong.com>
2019-09-05 17:24:21 +0800

    Update email

--- Config/email_config.py
+++ Config/email_config.py
@@ -11,12 +11,12 @@ mail_user_info = {
     'mws_user_receipt': 'amazonadmin@xcentz.com,xcentz1q2w3e4r5t@',
     'mws_user_invite': 'amazonservice@xcentz.com,44remg94SSkj',
     'native_user': 'nancy.deng@xcentz.com,denghua1991',
-    'mws_users': ['technicalsupport@xcentz.com,O_2cepytkcap',
-                  'info@xcentz.com,H_0gninthgil',
-                  'Proposal@xcentz.com,M_1retpadalevartlasrevinudp',
-                  'opinion@xcentz.com,G_9rotcetorpneercs',
-                  'advisory@xcentz.com,O_6esacrehtael',
-                  'QA@xcentz.com,D_6stesdaehhtooteulb',],
+    'mws_users': [# 'technicalsupport@xcentz.com,O_2cepytkcap',
+                  'info@xcentz.com,K]{d|4&c^jj/2?az:7q',
+                  'Proposal@xcentz.com,L0(5+\d8%/2@s!xx}sJ',
+                  'opinion@xcentz.com,Z1@3$r+-=p09*h%)./v',
+                  'advisory@xcentz.com,V88&r$4{!pPoi)d]>>7G',
+                  'QA@xcentz.com,T0*j^\||dSj&;"i0#s99',],
     'aws_user': 'AKIARQM6KYLJKBTG3Q7A,BFGF68VTgmn2fah9iUeYk4ryaBKIgGvDF+PDIwSckXN7',
     'pop_user': 'luomurong@yulong.com,YL50315yl'
 
